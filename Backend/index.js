869850800a6dcf6c007489693ab05d81c66b77fa
Azmat-7860 <azmatalli01234@gmail.com>
2024-11-21 02:27:16 +0530

    some cors problems

--- Backend/index.js
+++ Backend/index.js
@@ -7,15 +7,26 @@ const Authrouter = require("./Routes/AuthRouter");
 const app = express();
 const PORT = 8080;
 
+// Middleware to set custom CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Bodyparser middleware to parse JSON request bodies
 app.use(bodyparser.json());
+
+// CORS middleware to allow all origins
 app.use(
   cors({
-    origin: "https://mern-project-ui.vercel.app",
+    origin: "*", // Allow all origins
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   })
 );
 
+// Test route
 app.use("/hi", (req, res) => {
   res.send({ message: "Welcome Azmat!" });
   console.log("Success from server");
@@ -24,4 +35,5 @@ app.use("/hi", (req, res) => {
 // User routes
 app.use("/auth", Authrouter);
 
+// Start server
 app.listen(PORT, () => console.log("listening on port " + PORT));
